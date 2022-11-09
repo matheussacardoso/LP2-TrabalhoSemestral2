6eb0db8875386fadf52e1a6a8697e326d361861d
juauzitor <joao.vitor.mendes.pinto@gmail.com>
2022-11-09 09:17:58 -0300

    feat: database

---
+++ src/main/java/com/example/demo/model/Fornecedor.java
@@ -0,0 +1,69 @@
+package com.example.demo.model;
+
+public class Fornecedor {
+
+    private long id;
+    private String Nome;
+    private String Telefone;
+    private String CNPJ;
+
+    
+
+
+    /**
+     * @return long return the id
+     */
+    public long getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    /**
+     * @return String return the Nome
+     */
+    public String getNome() {
+        return Nome;
+    }
+
+    /**
+     * @param Nome the Nome to set
+     */
+    public void setNome(String Nome) {
+        this.Nome = Nome;
+    }
+
+    /**
+     * @return String return the Telefone
+     */
+    public String getTelefone() {
+        return Telefone;
+    }
+
+    /**
+     * @param Telefone the Telefone to set
+     */
+    public void setTelefone(String Telefone) {
+        this.Telefone = Telefone;
+    }
+
+    /**
+     * @return String return the CNPJ
+     */
+    public String getCNPJ() {
+        return CNPJ;
+    }
+
+    /**
+     * @param CNPJ the CNPJ to set
+     */
+    public void setCNPJ(String CNPJ) {
+        this.CNPJ = CNPJ;
+    }
+
+}
